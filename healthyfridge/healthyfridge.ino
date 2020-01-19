79e5d293a2339cccb1b34807245ba32911c53afe
Jean Decian <jeandecian@hotmail.fr>
2020-01-19 03:53:13 -0500

    JD : Piezo integration

--- healthyfridge/healthyfridge.ino
+++ healthyfridge/healthyfridge.ino
@@ -2,12 +2,16 @@
 #include <LiquidCrystal.h>
 
 #define DHT11_PIN       6
+#define BUZZER_PIN      9
 #define LED_PIN         13
 #define HCSR04_TRIGPIN  A2
 #define HCSR04_ECHOPIN  A3
 
 dht DHT;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+unsigned long start;
+bool humidityOK = false;
+bool temperatureOK = false;
 
 byte downArrow[8] = {
   0b00100, //   *
@@ -49,6 +53,8 @@ void message(String message, int col, int row, bool clearLcd) {
 
 void displayHumidityTemperature(int humidity, int temperature) {
   message("HUM  " + String(humidity) + " % ", 0, 0, true);
+  
+  humidityOK = false;
   Serial.print("H");
   if (humidity < 25) {
     lcd.write(byte(0));
@@ -58,10 +64,14 @@ void displayHumidityTemperature(int humidity, int temperature) {
     lcd.write(byte(1));
     lcd.print(" " + String(humidity - 40) + " %");
     Serial.print("H-");
+  } else {
+    humidityOK = true;
   }
   Serial.println(humidity);
   
   message("TEMP " + String(temperature) + (char)223 + "C ", 0, 1, false);
+
+  temperatureOK = false;
   Serial.print("T");
   if (temperature > 5) {
     lcd.write(byte(1));
@@ -70,8 +80,16 @@ void displayHumidityTemperature(int humidity, int temperature) {
   } else if (temperature == 0) {
     lcd.write(byte(0));
     Serial.print("L-");
+  } else {
+    temperatureOK = true;
   }
   Serial.println(temperature);
+
+  if (humidityOK && temperatureOK) {
+      digitalWrite(LED_PIN, LOW);
+  } else {
+      digitalWrite(LED_PIN, HIGH);
+  }
   
   delay(2000);
 }
@@ -82,21 +100,29 @@ void setup(){
   lcd.begin(16, 2);
   lcd.print("Loading");
   
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(HCSR04_TRIGPIN, OUTPUT);
   pinMode(HCSR04_ECHOPIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
   
   Serial.begin(9600);
 }
 
 void loop() {
+  start = millis();
   while(doorIsOpen()) {
-    digitalWrite(LED_PIN, HIGH);
+    if (millis() - start > 5000) {
+      tone(BUZZER_PIN, 300);
+      digitalWrite(LED_PIN, HIGH);
+      delay(500);
+      noTone(BUZZER_PIN);
+      digitalWrite(LED_PIN, LOW);
+      delay(500);
+    }
     message("Door open !", 0, 0, true);
     Serial.println("D-1");
     delay(100);
   }
-  digitalWrite(LED_PIN, LOW);
   
   int chk = DHT.read11(DHT11_PIN);
   displayHumidityTemperature(int(DHT.humidity), int(DHT.temperature));
