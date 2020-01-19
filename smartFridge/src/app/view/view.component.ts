e9beadd362653f3f637424c5afb11cc161785553
Thibault MESTON <thibault.meston@polymtl.ca>
2020-01-19 11:05:20 -0500

    complete display

--- smartFridge/src/app/view/view.component.ts
+++ smartFridge/src/app/view/view.component.ts
@@ -23,6 +23,7 @@ export class ViewComponent implements OnInit {
   protected stringProducts: string[];
   protected stringRecipe: string[];
   protected stringButton: string[];
+  protected colors: string[];
   constructor(private http: HttpClient) {
     this.recipeArray = new Array();
     this.productArray = new Array();
@@ -38,17 +39,19 @@ export class ViewComponent implements OnInit {
     this.stringButton = new Array();
     this.stringButton.push('add');
     this.stringButton.push('added');
+    this.colors = new Array();
+    this.colors.push('rgba(255, 0, 0, 0.5)');
+    this.colors.push('rgba(245, 171, 53, 1)');
+    this.colors.push('rgba(0, 255, 0, 0.5)');
   }
   ngOnInit() {
-    this.temperature = 5;
-
-    // setInterval(() => {
-    //   this.http.get('http://localhost:5000').subscribe((response: any) => {
-    //     this.temperature = 5;
-    //     this.humidity = response.H;
-    //     this.doorIsOpen = (response.D === 1);
-    //   });
-    // }, 3000);
+     setInterval(() => {
+       this.http.get('http://localhost:5000').subscribe((response: any) => {
+         this.temperature = response.T;
+         this.humidity = response.H;
+         this.doorIsOpen = (response.D === 1);
+       });
+    }, 3000);
   }
 
   goToRecipe(url: string) {
@@ -120,18 +123,26 @@ export class ViewComponent implements OnInit {
       });
   }
 
-  isDangerTemperature(): boolean {
-    return this.temperature < 1 || this.temperature > 4;
+  checkTemp(): number {
+    let tempState: number;
+    if (this.temperature < 1 || this.temperature > 5) {
+      tempState = 0;
+    } else if ( this.temperature === 1 || this.temperature === 5) {
+      tempState = 1;
+    } else {
+      tempState = 2;
+    }
+    return tempState;
   }
 
   checkHumidity(): number {
     let humidityState: number;
     if (this.humidity < 25 || this.humidity > 40) {
-      humidityState = 3;
+      humidityState = 0;
     } else if (this.humidity < 28 || this.humidity > 37) {
-      humidityState = 2;
-    } else {
       humidityState = 1;
+    } else {
+      humidityState = 2;
     }
     return humidityState;
   }
